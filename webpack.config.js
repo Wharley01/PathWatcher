03bdba7f5caa83394e535118f5f3296a56484bdd
Wharley01 <hackerslord96@gmail.com>
2019-02-06 01:03:41 +0100

    Added config files for js

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode:"development",
+  entry: './src/test.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
